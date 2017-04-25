7511e8b77fc4bbb1edba0195bfd4e0204ca2bf76
sam <morvazhu@gmail.com>
2017-04-25 08:05:48 +0800

    new and create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create]
+
+  def new
+    @qun = Qun.find(params[:qun_id])
+    @post = Post.new
+  end
+
+  def create
+    @qun = Qun.find(params[:qun_id])
+    @post = Post.new(post_params)
+    @post.qun = @qun
+    @post.user = current_user
+
+    if @post.save
+      redirect_to qun_path(@qun)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
